ed63d017ab80563951f00831653de07e6d1b43c1
matthew <matthew@debian>
2013-12-05 19:23:51 +0100

    delete non required files

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,11 +1,22 @@
 'use strict';
-
 /* Directives */
 
+angular.module('myApp.directives',['ngSanitize','myApp.services']).
+  directive('lorem', function(imageBuilder) {
+    return {
+	restrict: 'EA',
+	scope:{
+	  text:'@',
+	  count:'@',
+	  width:'@',
+	  height:'@'
+	},
+templateUrl:'partials/lorem-image.html',
+controller: function($scope) {
+$scope.images = imageBuilder.build($scope.text,
+		   $scope.count,
+		   $scope.height)
+}
+}
+});
 
-angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
-    return function(scope, elm, attrs) {
-      elm.text(version);
-    };
-  }]);
--- app/js/services.js
+++ app/js/services.js
@@ -2,8 +2,25 @@
 
 /* Services */
 
-
-// Demonstrate how to register services
-// In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+angular.module('myApp.services', []).service('imageBuilder',function() {
+  this.build = function(text,count,width,height){
+  var images = []
+  if (count == undefined) {
+      console.log('setting width to default value')
+      count = 1;
+  }
+  if (width == undefined) {
+      console.log('setting width to default value')
+      var width = '300';
+  }
+  if (height == undefined) {
+      var height = '200';
+      console.log('setting height to default value')
+  }
+  for (var i=0;i<count;i++) {
+    images.push({html:'<img src="http://lorempixel.com/'+width+'/'+height+'">'})
+  }
+  return images;
+  }
+})
+;
